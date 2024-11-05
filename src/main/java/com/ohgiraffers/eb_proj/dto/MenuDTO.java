ecbf7a21d9a6e0666412d07de8d28c9e698267f9
minseokKim6823 <alstjr6823@naver.com>
2024-11-05 10:26:14 +0900

    first commit

--- src/main/java/com/ohgiraffers/eb_proj/dto/MenuDTO.java
+++ src/main/java/com/ohgiraffers/eb_proj/dto/MenuDTO.java
@@ -1,4 +1,12 @@
 package com.ohgiraffers.eb_proj.dto;
 
+import lombok.Data;
+
+@Data
 public class MenuDTO {
+    private int menuCode;
+    private String menuName;
+    private String menuPrice;
+    private String categoryCode;
+    private String orderableStatus;
 }
--- src/main/java/com/ohgiraffers/eb_proj/service/MenuService.java
+++ src/main/java/com/ohgiraffers/eb_proj/service/MenuService.java
@@ -1,7 +1,11 @@
 package com.ohgiraffers.eb_proj.service;
 
+import com.ohgiraffers.eb_proj.dto.MenuDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MenuService {
+    public MenuDTO findMenuByMenuCode(int menuCode) {
+        return null;
+    }
 }
