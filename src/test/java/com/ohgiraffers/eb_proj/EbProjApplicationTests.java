6ada76a9a358d6548e9744482834f179871b1cef
minseokKim6823 <alstjr6823@naver.com>
2024-11-05 11:50:13 +0900

    Test

--- src/test/java/com/ohgiraffers/eb_proj/EbProjApplicationTests.java
+++ src/test/java/com/ohgiraffers/eb_proj/EbProjApplicationTests.java
@@ -1,13 +1,30 @@
 package com.ohgiraffers.eb_proj;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class EbProjApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("상태확인")
     @Test
-    void contextLoads() {
+    public void healthCheckTest() throws Exception{
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
+
 
 }
---
+++ src/test/java/com/ohgiraffers/eb_proj/controller/MenuControllerTests.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.eb_proj.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuControllerTests {
+
+}
